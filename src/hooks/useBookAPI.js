3c975c72f34fe6b0cad728bbf00fe8051b6d047b
Jonathan Pereira <96317160+JonathanPereira1993@users.noreply.github.com>
2024-05-04 10:43:04 +0100

    feat: Added another hook for BookAPI and .env file

---
+++ src/hooks/useBookAPI.js
@@ -0,0 +1,7 @@
+const apiKey = import.meta.env.REACT_APP_API_KEY;
+
+const useBookAPI = () => {
+  console.log(apiKey);
+};
+
+export default useBookAPI;
--- src/pages/Bookshelf/Bookshelf.jsx
+++ src/pages/Bookshelf/Bookshelf.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Banner from "../../components/Banner";
+import useBookAPI from "../../hooks/useBookAPI";
 
 const Bookshelf = () => {
+  useEffect(() => {
+    useBookAPI();
+  }, []);
+
   return (
     <div className="relative w-[90vw] sm:w-full max-w-widthScreen pt-[150px] sm:pt-[300px] mx-auto">
       <Banner
