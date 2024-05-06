63b1ce2ff9271d45f465b021cb7dae5365a2f68b
Jonathan Pereira <96317160+JonathanPereira1993@users.noreply.github.com>
2024-05-06 21:11:32 +0100

    feat:

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
-    include: /\.js$/,
+    include: /\.jsx$/,
     exclude: [],
     loader: "jsx",
   },
