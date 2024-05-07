6a1b87ba460f202bc7b8be9e5d3b4d1b47f265f5
Jonathan Pereira <96317160+JonathanPereira1993@users.noreply.github.com>
2024-05-07 07:40:27 +0100

    feat:

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
-    base: "/personal-portfolio-v2/",
     include: /\.jsx$/,
     exclude: [],
     loader: "jsx",
   },
   plugins: [react()],
+  base: "/personal-portfolio-v2/",
   define: {
     "process.env.VITE_EMAIL_JS_ID": JSON.stringify(
       process.env.VITE_EMAIL_JS_ID
